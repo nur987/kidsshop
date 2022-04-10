1d220beba3175ea1a1922189bae8c2db667785e2
Shaydiev Nurzhigit <75260064+nur987@users.noreply.github.com>
2022-04-10 11:33:19 +0600

    Commited

--- src/components/Header.js
+++ src/components/Header.js
@@ -57,16 +57,6 @@ const Header = () => {
                 </Link>
               </li>
               <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  to="#"
-                  id="navbarDropdownMenuLink"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown link
-                </Link>
                 <ul
                   className="dropdown-menu"
                   aria-labelledby="navbarDropdownMenuLink"
@@ -92,32 +82,6 @@ const Header = () => {
           </div>
         </div>
       </nav>
-      {/* <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-  <ol className="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img className="d-block w-100" src="https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_720/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/t9ur9cc1khkup1dmcbzd/%D0%9F%D0%B0%D1%80%D0%BA%D1%80%D0%B0%D0%B7%D0%B2%D0%BB%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D0%B9IMGWorldsofAdventure.jpg" alt="First slide"/>
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src="https://coasterpedia.net/w/images/thumb/3/3b/Velociraptor_%28IMG_Worlds_of_Adventure%29_2018_01.jpg/1000px-Velociraptor_%28IMG_Worlds_of_Adventure%29_2018_01.jpg" alt="Second slide"/>
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src="https://m.psecn.photoshelter.com/img-get/I0000G4GyGdjr2ag/s/1200/I0000G4GyGdjr2ag.jpg" alt="Third slide"/>
-    </div>
-  </div>
-  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="sr-only">Previous</span>
-  </a>
-  <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="sr-only">Next</span>
-  </a>
-</div> */}
     </div>
   );
 };

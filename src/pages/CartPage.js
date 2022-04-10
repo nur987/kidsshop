0722ac1fcfc470029ac1076e7e18cbee49081993
Shaydiev Nurzhigit <75260064+nur987@users.noreply.github.com>
2022-04-10 13:15:12 +0600

    done order modal, and order data send to firestore

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -2,10 +2,25 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Layout from "../components/Layout";
 import { FaTrash } from "react-icons/fa";
+import { Button, Modal } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { addDoc, collection } from "firebase/firestore";
+import fireDB from "../firebaseConfig";
+import { toast } from "react-toastify";
 const CartPage = () => {
   const { cartItems } = useSelector((state) => state.cartReducer);
   const [totalAmount, setTotalAmount] = useState(0);
   const dispatch = useDispatch();
+  //!react bootstrap for modal
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [pincode, setPincode] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     let temp = 0;
@@ -23,8 +38,36 @@ const CartPage = () => {
     dispatch({ type: "DELETE_FROM_CART", payload: product });
   };
 
+  const placeOrder = async () => {
+    const addressInfo = {
+      name,
+      address,
+      pincode,
+      phoneNumber,
+    };
+    console.log(addressInfo);
+
+    const orderInfo = {
+      cartItems,
+      addressInfo,
+      email: JSON.parse(localStorage.getItem("currentUser")).user.email,
+      userId: JSON.parse(localStorage.getItem("currentUser")).user.uid,
+    };
+    try {
+      setLoading(true);
+      const result = await addDoc(collection(fireDB, "orders"), orderInfo);
+      setLoading(false);
+      toast.success("order placed successfully");
+      handleClose();
+    } catch (error) {
+      setLoading(false);
+      console.log("error");
+      toast.error("Order failed");
+    }
+  };
+
   return (
-    <Layout>
+    <Layout loading={loading}>
       <table className="table mt-3">
         <thead>
           <tr>
@@ -60,8 +103,68 @@ const CartPage = () => {
         <h1 className="total-amount">Total Amount= {totalAmount} $</h1>
       </div>
       <div className="d-flex justify-content-end mt-0 mt-3">
-        <button>PLACE ORDER</button>
+        <button onClick={handleShow}>PLACE ORDER</button>
       </div>
+      {/* //!modal from react bootstrap */}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add your address</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="register-form">
+            <h2>Register</h2>
+            <hr />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Name..."
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            <textarea
+              type="text"
+              className="form-control"
+              rows={3}
+              defaultValue={""}
+              placeholder="Address..."
+              value={address}
+              onChange={(e) => {
+                setAddress(e.target.value);
+              }}
+            />
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Pincode..."
+              value={pincode}
+              onChange={(e) => {
+                setPincode(e.target.value);
+              }}
+            />
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Number..."
+              value={phoneNumber}
+              onChange={(e) => {
+                setPhoneNumber(e.target.value);
+              }}
+            />
+            <hr />
+            <Link to="/login"> Click herer to Login</Link>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={placeOrder}>
+            Order
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Layout>
   );
 };

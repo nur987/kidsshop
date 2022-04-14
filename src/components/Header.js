e23348bacc61f80f4f88146ffa4c923c3d848c25
Shaydiev Nurzhigit <75260064+nur987@users.noreply.github.com>
2022-04-14 23:56:55 +0600

    test netlify

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <Link className="navbar-brand text-black" to="/">
-            KidsShop
+            KidsSho
           </Link>
           <button
             className="navbar-toggler"
